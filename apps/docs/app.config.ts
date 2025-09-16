d645362dfc80ccdab927d640a5665ff8efaa8c55
CRBroughton <crbroughton@posteo.uk>
2025-09-16 22:07:23 +0100

    feat: :sparkles: add linter to docs workspace

---
+++ apps/docs/app.config.ts
@@ -0,0 +1,14 @@
+import { defineAppConfig } from 'nuxt/app'
+
+export default defineAppConfig({
+  ui: {
+    colors: {
+      primary: 'blue',
+      neutral: 'zinc',
+      success: 'green',
+      info: 'blue',
+      warning: 'orange',
+      error: 'red',
+    },
+  },
+})
